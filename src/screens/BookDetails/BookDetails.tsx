26139b3b11b2c7a1a49ae72f3a13a34ea747e2a9
Manu <manuabruzzo@gmail.com>
2021-09-15 19:22:42 -0300

    Book Screen Detail stylized

--- src/screens/BookDetails/BookDetails.tsx
+++ src/screens/BookDetails/BookDetails.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ActivityIndicator, ScrollView, View } from 'react-native';
+import { ActivityIndicator, Image, ScrollView, View } from 'react-native';
 
 import { CustomText, DefaultButton, Header, Separator } from '../../components';
 import { colors } from '../../utils/theme';
@@ -35,47 +35,60 @@ const BookDetailsScreen = ({ route }: { route: any }) => {
   return (
     <>
       <Header />
-      <View style={styles.mainContainer}>
-        {/* <Separator size={40} /> */}
-        <Separator />
-        <ScrollView>
-          <CustomText align="left" size={18} variant="bold">
-            Title
-          </CustomText>
-          <CustomText align="left" size={16}>
-            {book?.title}
-          </CustomText>
-          <Separator />
-          <CustomText align="left" size={18} variant="bold">
-            Author
-          </CustomText>
-          <CustomText align="left" size={16}>
-            {book?.author}
-          </CustomText>
-          <Separator />
-          <CustomText align="left" size={18} variant="bold">
-            Publish date
-          </CustomText>
-          {book?.publishDate[0].uk && (
-            <CustomText align="left" size={16}>
-              United Kingdom: {book?.publishDate[0].uk}
-            </CustomText>
-          )}
-          {book?.publishDate[1].us && (
-            <CustomText align="left" size={16}>
-              United States: {book?.publishDate[1].us}
-            </CustomText>
-          )}
-          <Separator />
-          <CustomText align="left" size={18} variant="bold">
-            Context years
-          </CustomText>
-          <CustomText align="left" size={16}>
-            {book?.plotTakePlaceYears.join(', ')}
-          </CustomText>
-          <Separator />
-        </ScrollView>
+      <Separator />
+      <View style={styles.title}>
+        <CustomText align="center" size={25} variant="bold" color={colors.red}>
+          {book?.title}
+        </CustomText>
       </View>
+
+      <ScrollView>
+        <View style={styles.mainContainer}>
+          <Separator />
+          <View style={styles.horizontalContainer}>
+            <Image source={{ uri: book?.bookCovers[0].url }} style={styles.image} resizeMode="contain" />
+            <Separator isHorizontal />
+            <View style={styles.infoConteinter}>
+              <CustomText align="left" size={16} variant="bold">
+                Author
+              </CustomText>
+              <CustomText align="left" size={14}>
+                {book?.author}
+              </CustomText>
+              <Separator />
+              <CustomText align="left" size={16} variant="bold">
+                Publish date
+              </CustomText>
+              {book?.publishDate[0].uk && (
+                <CustomText align="left" size={14}>
+                  United Kingdom: {book?.publishDate[0].uk}
+                </CustomText>
+              )}
+              {book?.publishDate[1].us && (
+                <CustomText align="left" size={14}>
+                  United States: {book?.publishDate[1].us}
+                </CustomText>
+              )}
+              <Separator />
+              <CustomText align="left" size={16} variant="bold">
+                Plot take place in years:
+              </CustomText>
+              <CustomText align="left" size={14}>
+                {book?.plotTakePlaceYears.join(', ')}
+              </CustomText>
+            </View>
+          </View>
+          <Separator size={20} />
+          <View style={styles.sinopsis}>
+            <CustomText align="justify" size={16} color={colors.black}>
+              Sinopsis?: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent venenatis porttitor orci at
+              pellentesque. Donec odio elit, placerat in nibh quis, mattis varius felis. Cras fermentum odio ut lectus
+              suscipit accumsan. Nam volutpat massa sagittis nunc consectetur, sit amet placerat enim vulputate. Donec
+              tempus ipsum nibh, sed aliquet mauris bibendum at.
+            </CustomText>
+          </View>
+        </View>
+      </ScrollView>
     </>
   );
 };
--- src/screens/BookDetails/styles.ts
+++ src/screens/BookDetails/styles.ts
@@ -1,16 +1,40 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../utils/theme';
 
 const styles = StyleSheet.create({
   mainContainer: {
+    alignSelf: 'center',
     alignContent: 'center',
     flex: 1,
-    width: '85%',
-    marginLeft: 30,
+    width: '90%',
+  },
+  title: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.yellow,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    width: '90%',
+    height: 110,
+  },
+  infoConteinter: {
+    justifyContent: 'center',
+  },
+  image: {
+    minWidth: 180,
+    height: 300,
+    borderRadius: 10,
   },
   horizontalContainer: {
     flexDirection: 'row',
+    alignSelf: 'center',
     justifyContent: 'center',
-    width: '100%',
+    width: '90%',
+  },
+  sinopsis: {
+    backgroundColor: colors.yellow,
+    borderRadius: 20,
+    padding: 15,
   },
   wholeScreenCenter: {
     alignItems: 'center',
